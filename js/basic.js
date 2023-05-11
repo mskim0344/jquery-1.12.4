19208354a64416aaea8a70e2e1dd13a515ab7dea
mskim0344 <mskim0344@gmail.com>
2023-05-11 11:37:33 +0900

    제이쿼리 알아보기

--- js/basic.js
+++ js/basic.js
@@ -1,1 +1,31 @@
-$(document).ready(function(){})git branch -M main
+$(document).ready(function(){})
+
+
+// $("#detail a").click(function(){})
+//아이디가 디테일이라는 객체의 a태그를 클릭하면 실행하라.
+// 선택자 이벤트 매서드가 제이쿼리의 핵심 구성이다.
+
+// $("#slider li:nth-child(1)").click(function(){})
+// 아이디가 슬라이더라는 객체의 첫번째 li태그를 클릭하면 실행하라.
+
+
+// $("#slider li:nth-child(1) > a").click(function(){})
+// 아이디 슬라이드 첫번째 li안의 자식 a태그를 클릭하면 실행하라
+
+// var num = 0;  
+// num = 1;          
+// $("#slider li").eq(0).find('>a').click(function(){})
+// 아이디 슬라이드 첫번째 li안의 자식 a태그를 클릭하면 실행하라
+
+// var num = 0;  넘이라는 공간에 0을 대입한다
+// num = 1;       넘을 1로 바꿧다 하지만 순차적으로 0에서 1로 바뀐다.
+
+// 알아둬야 하는 메서드
+// 주어용 메서드 : eq(숫자) , find(자식객체)
+// 동사용 메서드 : addclass(클래스명), removeclass(클래스명), toggleclass(클래스명)
+
+// 알아둬야 하는 메서드
+// ready : 선택자가 오직 하나 document
+// click : 모든 객체 해당
+// submit : 폼에 있는 botton, input만 해당
+// change : 폼에 있는 input만 해당
